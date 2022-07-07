ade35e5a4c568165b7e2ea4b56691a531d6e80dd
danialkarim807 <danialkarim8@gmail.com>
2022-07-07 14:43:10 +0500

    new commit

---
+++ index.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="images/logo.png">
+    <!-- custom font osome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- custome bootstreap style link -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- flickter csrosale scrolling link -->
+    <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
+    <!-- j query link -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <!-- styleling of css link -->
+    <link rel="stylesheet" href="css/style.css">
+    <title>Karakoram International University</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light"style="background-color:white;">
+        <div class="container-fluid"style="background-color:white;">
+            <a class="navbar-brand d-flex" href="index.php" >
+                <img height="100px" src="images/logo.png" alt="">
+                <h4 class="font-weight-bold heading-name" style="color:#070795;background-color:white; padding-top: 20px;">KARAKORAM
+                    INTERNATIONAL <br> UNIVERSITY</h4>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <!-- #070795 -->
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link font-weight-bold" href="index.php">Home <span
+                                class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link font-weight-bold" href="#about">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link font-weight-bold" href="#">Courses</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link font-weight-bold" href="#">Our Campus</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link font-weight-bold" href="#">Staff</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link font-weight-bold" href="#">Contact</a>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="container-fluid" style="">
+        <div class="row">
+            <div class="col-lg-12" style="padding: 0px;">
+            <img src="images/daniyal-01.jpg" class="img-fluid" alt="Responsive image"
+                    style="width: 100%; ">
+            </div>
+        </div>
+    </div>
+    
+
+    <div class="main-carousel">
+        <div class="cell"><img src="images/picture5.jpg"></div>
+        <div class="cell"><img src="images/picture6.jpg"></div>
+        <div class="cell"><img src="images/picture7.jpg"></div>
+        <div class="cell"><img src="images/picture10.jpg"></div>
+        <div class="cell"><img src="images/picture12.jpg"></div>
+
+    </div>
+    <div class="containeree">
+        <h3>About us</h3>
+        <img src="images/picture15.jpg" class="imgkiu">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate quidem quasi aut animi, odit ducimus
+            laudantium fugit laborum velit nihil itaque optio perferendis architecto ea qui commodi eligendi ex in vel
+            tempora ab. Voluptatum omnis possimus quae reiciendis asperiores totam, similique qui sit ullam corporis
+            quibusdam quis quaerat quam, ex voluptate cumque velit eligendi itaque corrupti eaque maiores! Voluptatibus
+            nesciunt veniam harum sapiente rerum cum doloremque nobis incidunt earum aliquid beatae consectetur non,
+            autem neque consequuntur dolores, voluptate iusto, quisquam repellat? Doloribus, deserunt, dolorem adipisci
+            sint, rerum inventore voluptatibus reiciendis unde sequi tempore aut nesciunt consequatur id provident quos
+            numquam blanditiis animi quia. Atque omnis quisquam ratione natus adipisci sapiente iusto ipsam, iure animi.
+            Veritatis architecto eveniet doloribus. Odio et, ea nesciunt optio eveniet delectus tempora vitae adipisci
+            vero ipsam labore odit quia esse consectetur repudiandae iusto magni, saepe, temporibus laborum? Quaerat eos
+            mollitia ex tempore quibusdam, magni officiis maiores ab sapiente dolore! Temporibus et nostrum, molestiae
+            laborum iure illum esse reprehenderit adipisci sint, quasi qui non velit voluptatum harum dolor consectetur
+            quia. Quod, quia quis. Quo quae est velit provident modi eum, amet ratione voluptate quisquam labore nihil
+            rerum quibusdam temporibus maiores exercitationem, sequi eveniet doloribus corporis doloremque reprehenderit
+            tempore! Nisi libero delectus, qui deserunt perspiciatis provident accusamus ea eligendi repellat doloribus
+            consectetur corrupti ex molestiae odio eveniet rem adipisci labore dolore porro iure assumenda est corporis.
+            Corrupti in quam reprehenderit aliquam neque optio sunt, eveniet dicta provident, aspernatur illo! Officia
+            sunt incidunt, eligendi mollitia voluptatem nesciunt ipsam aliquam pariatur libero aliquid omnis beatae
+            debitis, magnam laborum ab facilis unde quas. Ex incidunt voluptas optio eius accusamus voluptates ad
+            ducimus, nisi, quos eligendi illum fugit enim aspernatur non, itaque reprehenderit dicta. Necessitatibus,
+            laborum vero. Placeat quidem vel sapiente quibusdam a perferendis eos autem, corrupti, eius ab at iusto
+            ipsa!</p>
+
+    </div>
+    <div class="container" style=" padding-top:10px; ">
+        <div class="d-flex justify-content-around bg-dark">
+            <a class="btn btn-primary" style="background-color: #55acee;text-align:center;" href="#!" role="button"><i
+                    class="fab fa-twitter me-2"></i>Campus</a>
+
+            <a class="btn btn-primary" style="background-color: #55acee;text-align:center;" href="#!" role="button"><i
+                    class="fab fa-twitter me-2"></i>Managements</a>
+
+            <a class="btn btn-primary" style="background-color: #55acee;text-align:center;" href="#!" role="button"><i
+                    class="fab fa-twitter me-2"></i>Notification</a>
+
+            <a class="btn btn-primary" style="background-color: #55acee;text-align:center;" href="#!" role="button"><i
+                    class="fab fa-twitter me-2"></i>Students</a>
+
+        </div>
+
+    </div>
+    <div class="container-fluid " id="red" style="padding-top:30px ;">
+        <div class="row">
+            <div class="col-lg-6 bg-primary text-white " style="height: 300px; padding-top:10px; text-align:center; ">
+                row1Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                saepe quo, repellendus laborum impedit blanditiis eaque alias, dolorem vel reprehenderit quae iste ad,
+                quod rerum dignissimos! Blanditiis possimus vero quasi tenetur nihil repellendus harum! Inventore eos
+                nisi laboriosam suscipit consequatur molestias explicabo sed aut obcaecati ullam quisquam quas
+                voluptates magnam, illo repellat magni exercitationem sit. Architecto, exercitationem. Sed laborum,
+                necessitatibus eaque cumque nihil et ea aliquam voluptatum illo obcaecati assumenda eveniet vero nostrum
+                perferendis quisquam tenetur cupiditate laudantium accusantium autem. Iusto, laborum libero? Amet veniam
+                eius, eum delectus itaque maiores nostrum blanditiis cumque doloremque, dicta fugiat ex reprehenderit
+                corrupti sint
+            </div>
+            <div class="col-lg-6 bg-primary text-white">
+                <img src="images/logo.png" class="img-fluid" alt="Responsive image"
+                    style="padding-left: 70px; height:300px;">
+            </div>
+
+        </div>
+    </div>
+    <div class="decoration" style="background-color: blue; height:300px; padding-top:10px; clip-path: polygon(0 77%, 100% 100%, 100% 100%, 0% 100%);
+
+
+
+">
+
+    </div>
+
+
+
+    <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
+    <script type="text/javascript">
+    $('.main-carousel').flickity({
+        // options
+        cellAlign: 'left',
+        wrapArround: true,
+        freeScroll: true
+
+    });
+    </script>
+
+</body>
+
+</html>
